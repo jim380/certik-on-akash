aeca7cf33523179ea5c03ea733e636757dd6cafa
Jay Jie <jim380@pop-os.localdomain>
2021-04-15 13:59:59 -0700

    minor fixes

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ export CERTIK_HOME="{PWD?}"
 
 
 # This fails immediately, but creates the node keys
-certik init "${CERTIK_MONIKER:-unknown}" --home $CERTIK_HOME
+certik init "${CERTIK_MONIKER:-unknown}" --home $CERTIK_HOME/.certik
 
 set -xe
 
@@ -20,20 +20,20 @@ set -xe
 
 if test -n "$ENABLE_ID_SERVER" ; then
   mkdir web
-  certik tendermint show-node-id --home $CERTIK_HOME > web/node-id.txt
-  certik tendermint show-validator --home $CERTIK_HOME > web/validator-pubkey.txt
+  certik tendermint show-node-id --home $CERTIK_HOME/.certik > web/node-id.txt
+  certik tendermint show-validator --home $CERTIK_HOME/.certik > web/validator-pubkey.txt
   pushd web
   # Run a web server so that the file can be retrieved
   python3 -m http.server 8080 &
   popd
 fi
 
-curl -s "${GENESIS_URL?}" > $CERTIK_HOME/config/genesis.json
+curl -s "${GENESIS_URL?}" > $CERTIK_HOME/.certik/config/genesis.json
 
-cat config.toml | python3 -u ./patch_config_toml.py > $CERTIK_HOME/config/config.toml
+cat config.toml | python3 -u ./patch_config_toml.py > $CERTIK_HOME/.certik/config/config.toml
 
 # Copy over all the other filesthat the node needs
-cp -v app.toml $CERTIK_HOME/config/
+cp -v app.toml $CERTIK_HOME/.certik/config/
 
 # Run the node for real now 
-exec certik start --home $CERTIK_HOME
+exec certik start --home $CERTIK_HOME/.certik
